95af8a9c2d8f2e344c5b77bb87654fe04c85c49d
Patrick Colmenar <patrick.colmenar@gmail.com>
2016-02-10 23:01:36 -0800

    phase 2

---
+++ sketch_feb_phase2.ino
@@ -0,0 +1,47 @@
+#include <CapacitiveSensor.h>
+
+// Capacitive Keyboard + LED Matrix bubbles
+// by J.D. Zamfirescu
+// Last update: June 2014
+// capacitive_keyboard.ino
+
+#include <CapacitiveSensor.h>
+
+const int numSensors = 6;
+const int inputPins[] = {6, 7, 8, 9, 10, 11};
+
+//HT1632LEDMatrix matrix = HT1632LEDMatrix(DATA, WR, CS);
+CapacitiveSensor *sensors[6];
+
+void setup() {
+  for (int i = 0; i < numSensors; ++i) {
+    sensors[i] = new CapacitiveSensor(12, inputPins[i]);
+  }
+  Serial.begin(14400);
+}
+
+void loop() {
+  loadTimings();
+  printTimings();
+
+  delay(0);
+}
+
+long timings[] = {0, 0, 0, 0, 0, 0};
+
+void loadTimings() {
+  for (int i = 0; i < numSensors; ++i) {
+    timings[i] = sensors[i]->capacitiveSensor(10);
+  }
+}
+
+void printTimings() {
+  for (int i = 0; i < numSensors; ++i) {
+    if (i != 0) {
+      Serial.print(",");
+    }
+    Serial.print(timings[i]);
+  }
+  Serial.println();
+}
+
